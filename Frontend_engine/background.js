04e8121c8cf136cd36b580db2754ca0679e945db
Hen Mor <henmor93@gmail.com>
2020-09-03 14:26:22 +0300

    Create basic functions for steps sequence

--- Frontend_engine/background.js
+++ Frontend_engine/background.js
@@ -1,12 +1,9 @@
-var css = "body { border: 20px solid red; }";
-
 chrome.runtime.onMessage.addListener(injectCSS);
 
 
 function injectCSS (message, sender, senderFunc) {
     //inject css to the page
-    chrome.tabs.insertCSS({code: css});
-    //answer the script
-    senderFunc(sender.id);
+    chrome.tabs.insertCSS(null, {code: message});
+    senderFunc(message);
     
 }
--- Frontend_engine/player.js
+++ Frontend_engine/player.js
@@ -1,33 +1,108 @@
 //URL json guide
-let URL = "https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&refresh=true&env=dev&type=startPanel&vars%5Btype%5D=startPanel&sid=none&_=1582203987867";
+const URL = "https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&refresh=true&env=dev&type=startPanel&vars%5Btype%5D=startPanel&sid=none&_=1582203987867";
 
-let firstStep;
+let stepsArray;
+let currentStep;
+let currentStepID;
+let tootipDiv;
+let sttipDiv;
+let tiplates;
+let cssCode;
 
 //Fetching the data from the given endpoint
 fetch(URL).then(r => r.text()).then(result => {
 
-    // Result now contains the response text
+    //Result now contains the response text
     result = result.substring(11, result.length-1);
-    console.log(result);
 
     //Convert the data to js object
     result = JSON.parse(result);
+    stepsArray = result.data.structure.steps;
+    currentStepID = stepsArray[0].id;
+    tiplates = result.data.tiplates;
     
-    firstStep = result.data.structure.steps[0];
-
-    //Create new div for the first step
-    let div = document.createElement("div"); 
-    div.className = "temp";
-    document.body.appendChild(div); 
+    //CSS code from the data
+    cssCode = result.data.css;
+    loadCSS();
+    createDivContainer();
+    //create first step
+    createNewStep();
     
-    //Display first html element on the page
-    div.innerHTML= firstStep.action.contents["#content"];
-
 
 })
 
-//Connect to the background file
-chrome.runtime.sendMessage("temp", (funcToSend) =>{
-    console.log(funcToSend);
-  });
+
+function loadCSS(){
+
+    //Connect to the background file
+    chrome.runtime.sendMessage(cssCode.toString(), (funcToSend) =>{
+        console.log(funcToSend);
+
+    });
+
+
+}
+
+function createDivContainer(){
+
+    tootipDiv = document.createElement("div"); 
+    tootipDiv.className = "tooltip";
+    sttipDiv = document.createElement("div");
+    sttipDiv.className = "sttip";
+    sttipDiv.appendChild(tootipDiv);
+
+    //Display html element on the page
+    document.body.appendChild(sttipDiv); 
+
+   
+}
+
+function createNewStep(){
+
+    //find the current step
+    currentStep = stepsArray.find((step) => step.id === currentStepID)
+    
+    //last step
+    if(currentStep.action.type === "closeScenario"){
+        console.log("hereLastttt");
+        sttipDiv.remove();
+        return;
+    }
+
+    let htmlType = currentStep.action.type;
+
+    //assign html type for the current step
+    tootipDiv.innerHTML = tiplates[htmlType];
+
+    //brings every element which has attribute of this- for the content
+    document.querySelectorAll('[data-iridize-id=content]')[0].innerHTML = currentStep.action.contents["#content"];
+
+    //find next button
+    document.querySelectorAll('[data-iridize-role=nextBt]')[0].addEventListener("click", nextStep);
+    //find prev button
+    document.querySelectorAll('[data-iridize-role=prevBt]')[0].addEventListener("click", prevStep);
+
+
+}
+
+
+function nextStep(){
+
+    //update the stepIndex
+    currentStepID = currentStep.followers[0].next;
+    createNewStep();
+
+
+}
+
+function prevStep(){
+
+    
+}
+
+
+
+
+
+
 

7057b3d3516d0bfc5ff9a8d4e38e9c0eae6308b2
Hen Mor <henmor93@gmail.com>
2020-09-06 15:14:35 +0300

    Add package.json for tests

--- Frontend_engine/background.js
+++ Frontend_engine/background.js
@@ -4,9 +4,7 @@ chrome.runtime.onMessage.addListener(injectCSS);
 function injectCSS (message, sender, senderFunc) {
     //inject css to the page
     chrome.tabs.insertCSS(null, {code: message});
-    senderFunc(message);
  
-    
 }
 
 
